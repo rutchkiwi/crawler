7b2a9d7d2d314cb7727448cb2e6c53c3dc684c2f
Viktor Holmberg <viktor.holmberg@gmail.com>
2015-06-12 12:53:29 +0100

    first version where we returns almost right thing

--- crawler.go
+++ crawler.go
@@ -8,18 +8,18 @@ import (
 
 type queue struct{}
 
-func crawl(seedUrl string, fetcher Fetcher) chan string {
+func crawl(seedUrl string, fetcher Fetcher) chan siteInfo {
 	nonVisited := make(chan string, 100)    // todo should be lower
 	resultsChan := make(chan []string, 100) // todo should be lower
 
-	outputUrls := make(chan string, 100) // todo should be lower
+	outputAssets := make(chan siteInfo, 100) // todo should be lower
 
-	go dispatcher(nonVisited, resultsChan, outputUrls, seedUrl)
-	go processUrls(nonVisited, fetcher, resultsChan)
-	return outputUrls
+	go dispatcher(nonVisited, resultsChan, outputAssets, seedUrl)
+	go processUrls(nonVisited, fetcher, resultsChan, outputAssets)
+	return outputAssets
 }
 
-func dispatcher(nonVisited chan string, resultsChan chan []string, output chan<- string, seedUrl string) {
+func dispatcher(nonVisited chan string, resultsChan chan []string, outputAssets chan<- siteInfo, seedUrl string) {
 	// because we added a url before
 	seen := make(map[string]bool)
 	var wg sync.WaitGroup
@@ -33,7 +33,7 @@ func dispatcher(nonVisited chan string, resultsChan chan []string, output chan<-
 		wg.Wait()
 		close(nonVisited)
 		close(resultsChan)
-		close(output)
+		close(outputAssets)
 	}()
 
 	for res := range resultsChan {
@@ -43,7 +43,6 @@ func dispatcher(nonVisited chan string, resultsChan chan []string, output chan<-
 				seen[url] = true
 				wg.Add(1)
 				nonVisited <- url
-				output <- url
 			}
 		}
 		wg.Done()
@@ -51,16 +50,40 @@ func dispatcher(nonVisited chan string, resultsChan chan []string, output chan<-
 
 }
 
-func processUrls(nonVisited <-chan string, fetcher Fetcher, resultsChan chan<- []string) {
+type siteInfo struct {
+	url    string
+	assets []string
+}
+
+func processUrls(nonVisited <-chan string, fetcher Fetcher, resultsChan chan<- []string, outputAssets chan<- siteInfo) {
 	for url := range nonVisited {
+		// todo do assets and url search async ?
+
 		fmt.Printf("processing url %s \n", url)
 
 		body, _ := fetcher.Fetch(url) //handle error!
 
-		// fmt.Printf("body: %v", body)
+		fmt.Printf("body: %v", body)
+		assets := findAssets(body)
+		info := siteInfo{url, assets}
+		// bad name outputAssets
+		outputAssets <- info
+
 		urls := findUrls(body)
 		resultsChan <- urls
 	}
+}
+
+func findAssets(html string) []string {
+	r, _ := regexp.Compile(`src="()"`)
+	matches := r.FindAllStringSubmatch(html, -1)
+
+	res := make([]string, len(matches))
+	for i, match := range matches {
+		res[i] = match[1]
+	}
+
+	return res
 }
 
 func findUrls(html string) []string {
--- main_test.go
+++ main_test.go
@@ -6,15 +6,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCrawling(t *testing.T) {
-	var results chan string
-	results = crawl("http://a.com/", fakeFetcher)
+func TestCrawlsRightUrls(t *testing.T) {
+	var results chan siteInfo
+	results = crawl("http://a.com", fakeFetcher)
 	r1 := <-results
-	assert.Equal(t, "http://b.com/", r1)
+	assert.Equal(t, "http://a.com", r1.url)
+	r2 := <-results
+	assert.Equal(t, "http://b.com", r2.url)
+	// check empty now
 }
 
 // fetcher is a populated fakeFetcher.
 var fakeFetcher = FakeFetcher{
-	"http://a.com/": `<a href="http://b.com/"></a>`,
-	"http://b.com/": `<a href="http://a.com/"></a> bla bla bla <a href="http://b.com/">link text</a>`,
+	"http://a.com": `<a href="http://b.com"><img src="picA.png"</a>`,
+	"http://b.com": `<a href="http://a.com"></a><img src="picB.png"/><a href="http://b.com">link text</a>`,
 }
