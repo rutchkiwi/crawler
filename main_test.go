97e0493c2d6059a36341bdfb38668b2ade2702ed
Viktor Holmberg <viktor.holmberg@gmail.com>
2015-06-14 23:55:03 +0100

    fix and refactor main test

--- main_test.go
+++ main_test.go
@@ -7,91 +7,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCrawlsRightUrls(t *testing.T) {
-	var results chan SiteInfo
-	results, _ = crawl("http://gocardless.com/a", fakeFetcher1)
-
-	r1 := <-results
-	assert.Equal(t, "http://gocardless.com/a", r1.url)
-	r2, _ := <-results
-	assert.Equal(t, "https://gocardless.com/b", r2.url)
-	_, more := <-results
-	assert.False(t, more)
-}
-
-func TestFindImageAssets(t *testing.T) {
-	var results chan SiteInfo
-	results, _ = crawl("http://gocardless.com/a", fakeFetcher1)
-
-	r1 := <-results
-	assert.Equal(t, "http://gocardless.com/a", r1.url)
-	assert.Len(t, r1.assets, 1)
-	if len(r1.assets) == 1 {
-		assert.Equal(t, "picA.png", r1.assets[0])
-	}
-	r2, _ := <-results
-	assert.Equal(t, "https://gocardless.com/b", r2.url)
-	assert.Len(t, r2.assets, 1)
-	if len(r2.assets) == 1 {
-		assert.Equal(t, "picB.png", r2.assets[0])
-	}
-	_, more := <-results
-	assert.False(t, more)
-}
-
-func TestDontGoOutsideOriginalDomain(t *testing.T) {
-	var results chan SiteInfo
-	results, _ = crawl("http://gocardless.com/a", fakeFetcher2)
-
-	r1 := <-results
-	assert.Equal(t, "http://gocardless.com/a", r1.url)
-	assert.Len(t, r1.assets, 1)
-	if len(r1.assets) == 1 {
-		assert.Equal(t, "picA.png", r1.assets[0])
-	}
-	_, more := <-results
-	assert.False(t, more)
-}
-
-func TestRelativeUrls(t *testing.T) {
-	var results chan SiteInfo
-	results, _ = crawl("http://gocardless.com/a", fakeFetcherWithRelativeUrl)
-
-	r1 := <-results
-	assert.Equal(t, "http://gocardless.com/a", r1.url)
-	r2, _ := <-results
-	assert.Equal(t, "http://gocardless.com/b", r2.url)
-	_, more := <-results
-	assert.False(t, more)
-}
-
-func TestManyLinks(t *testing.T) {
-	// attempt to shake out concurrency issues
-	var results chan SiteInfo
-	results, _ = crawl("http://test.com/a", manyLinksFetcher)
-	urls := make([]string, 4)
-	r := <-results
-	urls = append(urls, r.url)
-	r = <-results
-	urls = append(urls, r.url)
-
-	r = <-results
-	urls = append(urls, r.url)
-	r = <-results
-	urls = append(urls, r.url)
-	fmt.Println(urls)
-	assert.Contains(t, urls, "http://test.com/a")
-	assert.Contains(t, urls, "http://test.com/b")
-	assert.Contains(t, urls, "http://test.com/c")
-	assert.Contains(t, urls, "http://test.com/d")
-	_, more := <-results
-	assert.False(t, more)
-}
-
-func TestError(t *testing.T) {
-	_, errors := crawl("http://nothingHere.se", fakeFetcher1)
-	err := <-errors
-	assert.EqualError(t, err, "not found: http://nothingHere.se")
+var fakeFetcher = FakeFetcher{
+	"http://gocardless.com/a":  `<a href="https://gocardless.com/b"><img src="picA.png"/><img src="picB.png"/></a>`,
+	"https://gocardless.com/b": `<a href="http://gocardless.com/a"></a><img src="picC.png"/><a href="https://gocardless.com/b">link text</a>`,
 }
 
 type FakeFetcher map[string]string
@@ -103,24 +21,87 @@ func (f FakeFetcher) Fetch(url string) (string, error) {
 	return "", fmt.Errorf("not found: %s", url)
 }
 
-var fakeFetcher1 = FakeFetcher{
-	"http://gocardless.com/a":  `<a href="https://gocardless.com/b"><img src="picA.png"/></a>`,
-	"https://gocardless.com/b": `<a href="http://gocardless.com/a"></a><img src="picB.png"/><a href="https://gocardless.com/b">link text</a>`,
+func TestCrawlsRightUrls(t *testing.T) {
+	urls := getUrlsFromCrawling(fakeFetcher, "http://gocardless.com/a")
+
+	assert.Contains(t, urls, "http://gocardless.com/a")
+	assert.Contains(t, urls, "https://gocardless.com/b")
+	assert.Len(t, urls, 2)
 }
 
-var fakeFetcher2 = FakeFetcher{
-	"http://gocardless.com/a": `<a href="https://google.com"><img src="picA.png"/></a>`,
-	"https://google.com":      `welcome to google <img src="google.png"/>`,
+func TestFindImageAssets(t *testing.T) {
+	assets := getAssetsFromCrawling(fakeFetcher, "http://gocardless.com/a")
+
+	assert.Len(t, assets, 2)
+	assert.Contains(t, assets, []string{"picA.png", "picB.png"})
+	assert.Contains(t, assets, []string{"picC.png"})
 }
 
-var fakeFetcherWithRelativeUrl = FakeFetcher{
-	"http://gocardless.com/a": `<a href="/b"><img src="picA.png"/></a>`,
-	"http://gocardless.com/b": `<a href="http://gocardless.com/a"></a><img src="picB.png"/><a href="http://gocardless.com/b">link text</a>`,
+func TestError(t *testing.T) {
+	_, errors := crawl("http://nothingHere.se", fakeFetcher)
+	err := <-errors
+	assert.EqualError(t, err, "not found: http://nothingHere.se")
 }
 
-var manyLinksFetcher = FakeFetcher{
-	"http://test.com/a": `<a href="/b"></a> <a href="/c"></a> <a href="/d"></a>`,
-	"http://test.com/b": `<a href="/1b"></a> <a href="/1c"></a> <a href="/1d"></a>`,
-	"http://test.com/c": `<a href="/b"></a> <a href="/c"></a> <a href="/d"></a>`,
-	"http://test.com/d": `<a href="/b"></a> <a href="/c"></a> <a href="/d"></a>`,
+func TestDontGoOutsideOriginalDomain(t *testing.T) {
+	var fakeFetcherWithLinkOutsideDomain = FakeFetcher{
+		"http://gocardless.com/a": `<a href="https://google.com"><img src="picA.png"/></a>`,
+		"https://google.com":      `welcome to google <img src="google.png"/>`,
+	}
+	urls := getUrlsFromCrawling(fakeFetcherWithLinkOutsideDomain, "http://gocardless.com/a")
+
+	assert.Len(t, urls, 1)
+	assert.Contains(t, urls, "http://gocardless.com/a")
+}
+
+func TestRelativeUrls(t *testing.T) {
+	var fakeFetcherWithRelativeUrl = FakeFetcher{
+		"http://gocardless.com/a": `<a href="/b"><img src="picA.png"/></a>`,
+		"http://gocardless.com/b": `<a href="http://gocardless.com/a"></a><img src="picB.png"/><a href="http://gocardless.com/b">link text</a>`,
+	}
+	urls := getUrlsFromCrawling(fakeFetcherWithRelativeUrl, "http://gocardless.com/a")
+
+	assert.Len(t, urls, 2)
+	assert.Contains(t, urls, "http://gocardless.com/a")
+	assert.Contains(t, urls, "http://gocardless.com/b")
+}
+
+func TestManyLinks(t *testing.T) {
+	var manyLinksFetcher = FakeFetcher{
+		"http://test.com/a": `<a href="/b"></a> <a href="/c"></a> <a href="/d"></a>`,
+		"http://test.com/b": `<a href="/1b"></a> <a href="/1c"></a> <a href="/1d"></a>`,
+		"http://test.com/c": `<a href="/b"></a> <a href="/c"></a> <a href="/d"></a>`,
+		"http://test.com/d": `<a href="/b"></a> <a href="/c"></a> <a href="/d"></a>`,
+	}
+	urls := getUrlsFromCrawling(manyLinksFetcher, "http://test.com/a")
+
+	assert.Len(t, urls, 4)
+	assert.Contains(t, urls, "http://test.com/a")
+	assert.Contains(t, urls, "http://test.com/b")
+	assert.Contains(t, urls, "http://test.com/c")
+	assert.Contains(t, urls, "http://test.com/d")
+
+}
+func getUrlsFromCrawling(fetcher Fetcher, url string) (urls []string) {
+	urls = make([]string, 0)
+
+	var results chan SiteInfo
+	results, _ = crawl(url, fetcher)
+
+	for r := range results {
+		urls = append(urls, r.url)
+	}
+	return urls
+}
+
+func getAssetsFromCrawling(fetcher Fetcher, url string) (assets [][]string) {
+	assets = make([][]string, 0)
+
+	var results chan SiteInfo
+	results, _ = crawl(url, fetcher)
+
+	for r := range results {
+		assets = append(assets, r.assets)
+	}
+	return assets
 }
